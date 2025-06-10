5684bba27c99050a05a5211ef4f615b98b0ab877
Ajith M <120308143+Ajith0245@users.noreply.github.com>
2025-06-10 10:34:27 +0530

    Delete src/main/java/com/dailycodework/dreamshops/exceptions directory

--- src/main/java/com/dailycodework/dreamshops/exceptions/InvalidCouponException.java
+++
@@ -1,7 +0,0 @@
-package com.dailycodework.dreamshops.exceptions;
-
-public class InvalidCouponException extends RuntimeException {
-    public InvalidCouponException(String message) {
-        super(message);
-    }
-}
